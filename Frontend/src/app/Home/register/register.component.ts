a0a1bec8d1886ac6a307a868a29a038689c8da8d
Thejas200 <71145680+Thejas200@users.noreply.github.com>
2025-01-17 23:22:10 +0100

    Add files via upload

---
+++ Frontend/src/app/Home/register/register.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HotToastService } from '@ngneat/hot-toast';
+import { ApiService } from 'src/app/services/api.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+
+  registerForm:any;
+  payload:any;
+  response:any;
+  user:any; 
+  constructor(private api:ApiService,private router:Router,private fb:FormBuilder,private toast:HotToastService) { }
+
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      username:["",[Validators.required]],
+      email:["",[Validators.required]],
+      password:["",[Validators.required]],
+      checkPassword:["",[Validators.required]]
+    })
+
+   
+  }
+
+  register(){
+    this.payload = this.registerForm.value;
+    console.log(this.payload)
+    
+    this.api.post("/register",this.payload).subscribe(
+      next => {
+        console.log(next)
+        this.toast.success("User registered successfully")
+        this.router.navigate(["login"])
+      },
+      error => {
+        console.log(error)
+        this.toast.error(error.error)
+      }
+    )
+  }
+
+}
